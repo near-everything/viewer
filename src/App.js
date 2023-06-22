991d043c7bbd3b3980320eeb47eb18c95da708ed
Elliot Braem <16282460+elliotBraem@users.noreply.github.com>
2023-06-21 20:35:25 -0400

    feature: adds Scanner to VM

--- src/App.js
+++ src/App.js
@@ -79,6 +79,9 @@ function App(props) {
             }
             return <Link {...props} />;
           },
+          Scanner: (props) => {
+            return <Scanner {...props} />;
+          }
         },
       });
   }, [initNear]);
