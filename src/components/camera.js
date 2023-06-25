164ef5b6e4fec72f2f61044ed89508d49fa00bdb
Elliot Braem <16282460+elliotBraem@users.noreply.github.com>
2023-06-25 15:18:57 -0400

    feature: adds capture photo

--- src/components/camera.js
+++ src/components/camera.js
@@ -1,14 +1,14 @@
 import React, { useEffect, useRef, useState } from "react";
 import Webcam from "react-webcam";
+import styled from "styled-components";
 
-// const Content = styled.div`
-//   position: absolute;
-//   left: 50%;
-//   top: 50%;
-//   -webkit-transform: translate(-50%, -50%);
-//   transform: translate(-50%, -50%);
-//   text-align: center;
-// `;
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 0 auto;
+  height: 100%;
+`;
 
 export const Camera = (props) => {
   const webcamRef = useRef(null);
@@ -22,10 +22,10 @@ export const Camera = (props) => {
       setWindowSize([window.innerWidth, window.innerHeight]);
     };
 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
 
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
 
@@ -34,23 +34,53 @@ export const Camera = (props) => {
     width: windowSize[0],
     height: windowSize[1],
   };
-  
+
+  const capture = React.useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    props.onCapture(imageSrc); // Here's the function you passed through the props
+  }, [webcamRef, props.onCapture]);
+
   return (
-    <Webcam
-      audio={props.audio || false}
-      width={props.width || "100%"}
-      height={props.height || "100%"}
-      ref={webcamRef}
-      screenshotFormat="image/jpeg"
-      videoConstraints={videoConstraints}
-      style={{
-        position: 'absolute',
-        right: '0',
-        bottom: '0',
-        minWidth: '100%',
-        minHeight: '100%',
-      }}
-      mirrored
-    />
+    <Container>
+      <div style={{position: 'relative', width: '100%', height: '100%'}}>
+        <Webcam
+          audio={props.audio || false}
+          width={props.width || "100%"}
+          height={props.height || "100%"}
+          ref={webcamRef}
+          screenshotFormat="image/jpeg"
+          videoConstraints={videoConstraints}
+          style={{
+            position: 'absolute',
+            right: '0',
+            bottom: '0',
+            height: '100%'
+          }}
+          // style={{
+          //   position: "absolute",
+          //   right: "0",
+          //   bottom: "0",
+          //   minWidth: "100%",
+          //   minHeight: "100%",
+          // }}
+          mirrored={props.facingMode === FACING_MODE_USER ? true : false}
+        />
+      </div>
+      <button 
+        onClick={capture} 
+        style={{
+          position: 'absolute', 
+          bottom: '10px', 
+          left: '50%', 
+          transform: 'translateX(-50%)', 
+          backgroundColor: 'white', 
+          padding: '10px', 
+          borderRadius: '5px', 
+          border: 'none',
+        }}
+      >
+        Capture photo
+      </button>
+    </Container>
   );
 };
