51435faf45f4e1617fc423c3dc40e2b5a9ec1515
sekaiking <afaithraf@gmail.com>
2023-08-15 01:51:51 +0800

    Update sign in button

--- src/components/navigation/SignInButton.js
+++ src/components/navigation/SignInButton.js
@@ -1,10 +1,24 @@
 import React from "react";
-import { GrayBorderButton } from "../common/buttons/GrayBorderButton";
+import styled from "styled-components";
+
+const Button = styled.button`
+  height: 40px;
+  border-radius: 100px;
+  border: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0 24px;
+  font-size: 14px;
+  font-weight: 500;
+`;
 
 export function SignInButton(props) {
   return (
-    <button onClick={props.onSignIn} style={{ padding: "12px", fontWeight: "var(--font-weight-medium)" }}>
+    <Button
+      onClick={props.onSignIn}
+    >
       Sign In
-    </button>
+    </Button>
   );
 }
