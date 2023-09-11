712c23153e9e64bd9bfd2da9b79b2fe08190f1cd
nataliakif <nataliakif@gmail.com>
2023-09-11 13:38:25 +0300

    1

--- src/components/custom/TeleportGenerator.js
+++ src/components/custom/TeleportGenerator.js
@@ -1,41 +1,26 @@
+import { createReactComponentGenerator } from "@teleporthq/teleport-component-generator-react";
+import { createBOSComponentGenerator } from "@teleporthq/teleport-component-generator-bos";
 import React from "react";
-import {
-  createReactComponentGenerator,
-  ReactMapping,
-} from "@teleporthq/teleport-component-generator-react";
-import styledComponentsPlugin from "@teleporthq/teleport-plugin-react-styled-components";
-import cssPlugin from "@teleporthq/teleport-plugin-css";
-import splitReactCode from "./TeleportFunctions";
-
-const customMapping = {
-  ...ReactMapping,
-  elements: {
-    img: {
-      elementType: "widget",
-    },
-  },
-  elements: {
-    nav: {
-      elementType: "div",
-    },
-  },
-  events: {
-    click: "onclick",
-  },
-
-  illegalClassNames: [...ReactMapping.illegalClassNames, "nav"],
-};
 
 export const TeleportGenerator = (props) => {
-  async function generate() {
+  async function generateReact() {
     try {
       const generator = createReactComponentGenerator();
-      generator.addMapping(customMapping);
-      generator.addPlugin(styledComponentsPlugin);
       const uidl = props.getUIDL();
       const { files } = await generator.generateComponent(JSON.parse(uidl));
-      props.onGenerate({ code: splitReactCode(files[0].content) });
-      //props.onGenerate({ code: files[0].content });
+      props.onGenerate({ code: files[0].content });
+    } catch (e) {
+      props.onGenerate({ error: e.message });
+      console.error("error while generating component from uidl: " + e);
+    }
+  }
+
+  async function generateBOS() {
+    try {
+      const generator = createBOSComponentGenerator();
+      const uidl = props.getUIDL();
+      const { files } = await generator.generateComponent(JSON.parse(uidl));
+      props.onGenerate({ code: files[0].content });
     } catch (e) {
       props.onGenerate({ error: e.message });
       console.error("error while generating component from uidl: " + e);
