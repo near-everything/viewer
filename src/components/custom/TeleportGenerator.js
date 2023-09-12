7a0d02a94a8b164378afc8700c071f05ff7e636d
nataliakif <nataliakif@gmail.com>
2023-09-12 12:14:06 +0300

    add local plagin

--- src/components/custom/TeleportGenerator.js
+++ src/components/custom/TeleportGenerator.js
@@ -1,5 +1,5 @@
 import { createReactComponentGenerator } from "@teleporthq/teleport-component-generator-react";
-import { createBOSComponentGenerator } from "@teleporthq/teleport-component-generator-bos";
+// import { createBOSComponentGenerator } from "@teleporthq/teleport-component-generator-bos";
 import React from "react";
 
 export const TeleportGenerator = (props) => {
@@ -15,17 +15,17 @@ export const TeleportGenerator = (props) => {
     }
   }
 
-  async function generateBOS() {
-    try {
-      const generator = createBOSComponentGenerator();
-      const uidl = props.getUIDL();
-      const { files } = await generator.generateComponent(JSON.parse(uidl));
-      props.onGenerate({ code: files[0].content });
-    } catch (e) {
-      props.onGenerate({ error: e.message });
-      console.error("error while generating component from uidl: " + e);
-    }
-  }
+  // async function generateBOS() {
+  //   try {
+  //     const generator = createBOSComponentGenerator();
+  //     const uidl = props.getUIDL();
+  //     const { files } = await generator.generateComponent(JSON.parse(uidl));
+  //     props.onGenerate({ code: files[0].content });
+  //   } catch (e) {
+  //     props.onGenerate({ error: e.message });
+  //     console.error("error while generating component from uidl: " + e);
+  //   }
+  // }
 
   return <button onClick={generate}>Generate</button>;
 };
