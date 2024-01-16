06a4242e5c2aa39fc886c7bdc569f59d1dd8f43c
Elliot Braem <16282460+elliotBraem@users.noreply.github.com>
2024-01-16 14:09:07 -0500

    adds layout

--- apps/main/widget/app.jsx
+++ apps/main/widget/app.jsx
@@ -3,9 +3,9 @@
  */
 const { page, layout, loading, routes, ...passProps } = props;
 
-// const { AppLayout } =
-//   VM.require("devhub.near/widget/devhub.components.templates.AppLayout") ||
-//   (() => {});
+const { AppLayout } = VM.require("every.near/widget/layout") || {
+  AppLayout: () => <>hello</>,
+};
 
 if (!page) page = "home";
 
@@ -60,98 +60,20 @@ const Container = styled.div`
   height: 100vh;
 `;
 
-const Sidebar = styled.div`
-  width: 64px;
-
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-  padding: 20px 0;
-
-  border: 2px outset #333;
-  background-color: #f5f5f5;
-`;
-
-const ButtonGroup = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 8px;
-`;
-
-const Button = styled.button`
-  width: 48px;
-  min-height: 48px;
-
-  border: 2px outset #333;
-  background-color: #f5f5f5;
-  cursor: pointer;
-  color: #333;
-
-  text-decoration: none;
-
-  &:active {
-    border-style: inset;
-    background-color: #d5d5d5;
-    color: #000;
-  }
-
-  &:hover {
-    background-color: #e5e5e5;
-    color: #111;
-  }
-`;
-
 const Content = styled.div`
   width: 100%;
   height: 100%;
   overflow: scroll;
 `;
 
-const [activeRoute, setActiveRoute] = useState(page);
-
-const handleLinkClick = (path, passProps) => {
-  setActiveRoute(path);
-  setExtraProps(passProps);
-};
-
 return (
-  <Container>
-    <Sidebar>
-      <ButtonGroup
-        style={{ maxHeight: "calc(100% - 50px)", overflow: "scroll" }}
-      >
-        {(Object.keys(routes) || []).map((k) => {
-          const route = routes[k];
-          if (route.hide) {
-            return null;
-          }
-          return (
-            <Button key={k} onClick={() => handleLinkClick(k)}>
-              <i className={route.init.icon}></i>
-            </Button>
-          );
-        })}
-      </ButtonGroup>
-      <ButtonGroup style={{ marginTop: "8px" }}>
-        <Button
-          onClick={() => handleLinkClick("inspect", { src: routes[activeRoute].path })}
-        >
-          <i className={"bi bi-code"}></i>
-        </Button>
-        <Button
-          onClick={() =>
-            handleLinkClick("notifications")
-          }
-        >
-          <i className={"bi bi-bell"}></i>
-        </Button>
-        <div style={{ width: "48px", height: "48px" }}></div>
-      </ButtonGroup>
-    </Sidebar>
-    <Content>
-      <Router active={activeRoute} routes={routes} />
-    </Content>
-  </Container>
+  <Theme> 
+    <Container>
+      <AppLayout page={route} routes={routes}>
+        <Content>
+          <Router active={activeRoute} routes={routes} />
+        </Content>
+      </AppLayout>
+    </Container>
+  </Theme>
 );
