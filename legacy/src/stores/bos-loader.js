aa0ec3f7077f957465542a207b1b9042b02a3f90
Elliot Braem <16282460+elliotBraem@users.noreply.github.com>
2024-03-16 12:51:23 -0400

    cleanup

--- legacy/src/stores/bos-loader.js
+++
@@ -1,9 +0,0 @@
-import { create } from 'zustand';
-
-export const useBosLoaderStore = create((set) => ({
-  failedToLoad: false,
-  hasResolved: false,
-  loaderUrl: '',
-  redirectMap: {},
-  set: (state) => set((previousState) => ({ ...previousState, ...state })),
-}));
