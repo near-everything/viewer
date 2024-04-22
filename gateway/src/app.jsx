86af83fa0b07e02f749ec92a39fa081bb2de5a12
Elliot Braem <16282460+elliotBraem@users.noreply.github.com>
2024-04-22 10:16:47 -0400

    vite gateway

--- gateway/src/app.jsx
+++
@@ -1,5 +0,0 @@
-import "./app.css";
-
-export function App() {
-  return <near-social-viewer src="efiz.near/widget/Tree" />;
-}
---
+++ src/app.jsx
@@ -0,0 +1,5 @@
+import "./app.css";
+
+export function App() {
+  return <near-social-viewer src="trylivepeer.near/widget/index" />;
+}
